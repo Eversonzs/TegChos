f22d7adda241394d1fcdcc6af46ea635c6b1a1cc
LuiisM22 <LuiisM22@hotmail.com>
2015-11-08 06:02:48 -0600

    Ingreso Solicitudes

--- app/TegChos/app/src/main/java/hn/tictanes/tegchos/IngresoSolicitudFragment.java
+++ app/TegChos/app/src/main/java/hn/tictanes/tegchos/IngresoSolicitudFragment.java
@@ -1,10 +1,17 @@
 package hn.tictanes.tegchos;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.PopupWindow;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -18,6 +25,45 @@ public class IngresoSolicitudFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_ingreso_solicitud, container, false);
+
+        Button btnEnviar = (Button) (rootView.findViewById(R.id.btnEnviarSolicitud));
+        ImageButton imgButton = (ImageButton) (rootView.findViewById(R.id.imgButton));
+
+        imgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case DialogInterface.BUTTON_POSITIVE:
+
+                                break;
+                            case DialogInterface.BUTTON_NEGATIVE:
+
+                                break;
+                        }
+                    }
+                };
+
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setMessage("Seleccione una opcion...")
+                        .setPositiveButton("Foto", dialogClickListener)
+                        .setNegativeButton("Galeria", dialogClickListener)
+                        .setTitle("Cargar foto")
+                        .show();
+            }
+        });
+
+
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Hola", Toast.LENGTH_SHORT).show();
+            }
+        });
         return rootView;
     }
 }
---
+++ app/TegChos/app/src/main/java/hn/tictanes/tegchos/PopUp.java
@@ -0,0 +1,10 @@
+package hn.tictanes.tegchos;
+
+/**
+ * Created by LuisAntonio on 08/11/2015.
+ */
+public class PopUp {
+
+
+
+}
